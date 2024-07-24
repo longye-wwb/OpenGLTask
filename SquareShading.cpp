c2fd34e569281620e49ad9786d24dffe98a4a5f2
longye-wwb <764743604@qq.com>
2024-07-24 10:07:21 +0800

    代码简化

--- SquareShading.cpp
+++ SquareShading.cpp
@@ -1,34 +1,33 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+#include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
 
 #include "shader_s.h"
-#include "camera.h"
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 int main()
 {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* Window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
 
-    if (window == NULL)
+    if (Window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwMakeContextCurrent(Window);
+    glfwSetFramebufferSizeCallback(Window, framebuffer_size_callback);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -38,14 +37,14 @@ int main()
 
     Shader LightingShader("directionLight.vs", "directionLight.fs");
 
-    float vertices[] = {
+    float Vertices[] = {
         //顶点              颜色             法线
         0.5f,  0.5f, 0.0f,  1.0f,0.0f,0.0f, 0.0f,0.0f,1.0f,
          0.5f, -0.5f, 0.0f, 0.0f,1.0f,0.0f,0.0f,0.0f,1.0f,
         -0.5f, -0.5f, 0.0f,0.0f,0.0f,1.0f,0.0f,0.0f,1.0f,
         -0.5f,  0.5f, 0.0f ,0.0f,0.0f,0.0f,0.0f,0.0f,1.0f,
     };
-    unsigned int indices[] = {
+    unsigned int Indices[] = {
         0, 1, 3,
         1, 2, 3
     };
@@ -57,10 +56,10 @@ int main()
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -71,22 +70,26 @@ int main()
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    while (!glfwWindowShouldClose(window))
+    glm::vec3 CameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 Front = glm::vec3(0.0f, 0.0f, -1.0f);
+
+    while (!glfwWindowShouldClose(Window))
     {
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         LightingShader.use();
         LightingShader.setVec3("direction", 1.0f, -1.0f, -1.0f);
-        LightingShader.setVec3("viewPos", camera.Position);
+        LightingShader.setVec3("viewPos", CameraPos);
         LightingShader.setFloat("shininess", 32.0f);
         LightingShader.setVec3("lightColor", 0.3f, 0.3f, 0.3f);
         LightingShader.setFloat("ambientStrength", 0.1f);
 
-        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = camera.GetViewMatrix();
-        LightingShader.setMat4("uProjection", projection);
-        LightingShader.setMat4("uView", view);
+        glm::mat4 ProjectionMat = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 ViewMat = glm::lookAt(CameraPos, CameraPos + Front, Up);
+        LightingShader.setMat4("uProjection", ProjectionMat);
+        LightingShader.setMat4("uView", ViewMat);
 
         glm::mat4 model = glm::mat4(1.0f);
         LightingShader.setMat4("uModel", model);
@@ -94,7 +97,7 @@ int main()
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(Window);
         glfwPollEvents();
     }
 
